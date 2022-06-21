f023a371a65696244cd97f920675675f3acbd60b
Denis <Kds04042002omgu@gmail.com>
2022-06-22 00:45:30 +0600

    Добавьте файлы проекта.

---
+++ SharpFirstTask/LeapYearCheck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpFirstTask
+{
+    public class LeapYearCheck
+    {
+        public static bool LeapYearCheckFunc(int Year)
+        {
+            if (Year % 4 == 0 && Year % 100 != 0)
+                return true;
+            else if (Year % 400 == 0)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ SharpFirstTask/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите год: ");
+        string input = Console.ReadLine();
+        int Year = int.Parse(input);
+        
+       if(SharpFirstTask.LeapYearCheck.LeapYearCheckFunc(Year))
+        Console.WriteLine(Year + " - Високосный год");
+       else
+        Console.WriteLine(Year + " - Не високосный год");
+    }
+}
